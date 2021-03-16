04c53acc51f41ce7e0898d922d7e47f19d597874
sarah-knowles <sjk339@gmail.com>
2021-03-16 18:22:53 +1300

    sets up components

---
+++ movies-watchlist/README.md
@@ -0,0 +1,1 @@
+# React App Movie Watchlist
---
+++ movies-watchlist/src/Components/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Header } from './Header';
+import { Watchlist } from './Watchlist';
+import { Watched } from './Watched';
+import { Add } from './Add';
+
+import '../index.css';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Switch>
+
+        <Route exact path='/'>
+          <Watchlist />
+        </Route>
+
+        <Route path='/watched'>
+          <Watched />
+        </Route>
+
+        <Route path='/add'>
+          <Add />
+        </Route>
+
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
